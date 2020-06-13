24ee7d4aa4beadaa36c8660af30c1134effa3eee
liaramosg <lia.ramosg@outlook.com>
2020-06-13 17:27:37 -0400

    se sube primera versión del código

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
-import validator from './validator.js';
+//BOTON
+document.getElementById("Validation_button").addEventListener("click", function(){Result()});
 
-console.log(validator);
+//ACCION
+Result = () => {
+    var n_card = document.getElementById("number").value.replace(/\s+/g, '');
+    if (!/\D/.test(n_card)) {
+        if (n_card.length == 16) {
+            document.getElementById("number").value = maskify(n_card);
+            document.getElementById("result").innerHTML = isValid(n_card);
+            verResultados.style.display = "block";
+        } else {
+            window.alert("Ingrese 16 digitos por favor")
+        }
+    } else {
+        window.alert("Ingrese solo numeros por favor")
+    }
+}
+
+
